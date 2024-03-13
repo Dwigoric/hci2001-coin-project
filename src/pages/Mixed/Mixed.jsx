6a257aff39dbeeb07b4f6bc3020ff9aa70c79c54
Joshua Permito <dwigoric@insanedevs.com>
2024-03-13 17:31:44 +0800

    feat: add page for mixed design

--- src/pages/Mixed/Mixed.jsx
+++ src/pages/Mixed/Mixed.jsx
@@ -1,8 +1,55 @@
+// Package imports
+import { useState } from 'react'
+
+// Stylesheets
+import styles from './Mixed.module.css'
+
+// Components
+import ProfilePanel from '../../components/ProfilePanel/ProfilePanel.jsx'
+import ButtonTab from '../../components/ButtonTab/ButtonTab.jsx'
+
+// Utils
+import Tabs from '../../util/tabs.jsx'
+
 function Mixed() {
+    const newButtons = Object.keys(Tabs).map((tab, i) => {
+        return (
+            <ButtonTab
+                key={i}
+                buttonId={i}
+                text={tab}
+                setActive={changeActiveTab}
+                isActive={i === 0}
+            />
+        )
+    })
+
+    const [firstLevelButtons, setFirstLevelButtons] = useState(newButtons)
+
+    function changeActiveTab(index) {
+        const buttons = Object.keys(Tabs).map((tab, i) => {
+            return (
+                <ButtonTab
+                    key={i}
+                    buttonId={i}
+                    text={tab}
+                    setActive={changeActiveTab}
+                    isActive={i === index}
+                />
+            )
+        })
+        setFirstLevelButtons(buttons)
+    }
+
     return (
-        <>
-            <p>Mixed</p>
-        </>
+        <div className={styles.mixedPage}>
+            <div className={styles.settingsPanel}>
+                <ProfilePanel />
+                <div className={styles.firstLevelTabs}>
+                    {firstLevelButtons}
+                </div>
+            </div>
+        </div>
     )
 }
 
