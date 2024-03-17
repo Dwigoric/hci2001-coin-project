0870479d83ee5db888e659fbbfed97161a67df8b
Joshua Permito <dwigoric@insanedevs.com>
2024-03-17 19:00:05 +0800

    feat: add logger utility class

---
+++ src/util/logger.js
@@ -0,0 +1,30 @@
+class Logger {
+    constructor() {
+        this.logs = []
+    }
+
+    log({ action, message }) {
+        if (!action) {
+            throw new Error('action is required')
+        }
+        if (!message) {
+            throw new Error('message is required')
+        }
+
+        const timestamp = Date.now()
+        const timeDelta = this.logs.length
+            ? timestamp - this.logs[this.logs.length - 1].timestamp
+            : 0
+        this.logs.push({ action, timestamp, timeDelta, message })
+    }
+
+    printLogCount() {
+        console.log(`${this.logs.length} logs`)
+    }
+
+    exportAsJSON() {
+        return JSON.stringify(this.logs)
+    }
+}
+
+export default Logger
