c8c1b94a4adf3e9981c837cfa5c298a2ed3341f9
Joshua Permito <dwigoric@insanedevs.com>
2024-03-17 19:57:59 +0800

    feat: add button to export collected data

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,47 @@
+// Package imports
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import routes from './routes/routes'
 
+// Utilities
+import logger from './util/logger.js'
+
+// Function to export collected data
+function exportData() {
+    const data = logger.exportAsJSON()
+
+    const blob = new Blob([data], { type: 'application/json' })
+    const link = document.createElement('a')
+    link.download = 'collected-data.json'
+    link.href = window.URL.createObjectURL(blob)
+    link.dataset.downloadurl = ['application/json', link.download, link.href].join(':')
+
+    const event = new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true
+    })
+
+    link.dispatchEvent(event)
+    window.URL.revokeObjectURL(link.href)
+    link.remove()
+}
+
 function App() {
-  return (
-    <>
-      <Routes>
-        {routes.map((route, index) => (
-          <Route
-            path={route.path}
-            element={route.element}
-            key={index}
-          />
-        ))}
-      </Routes>
-    </>
-  )
+    return (
+        <>
+            <Routes>
+                {routes.map((route, index) => (
+                    <Route
+                        path={route.path}
+                        element={route.element}
+                        key={index}
+                    />
+                ))}
+            </Routes>
+            <button id="export-button" onClick={exportData}>Export Collected Data</button>
+        </>
+    )
 }
 
 export default App
