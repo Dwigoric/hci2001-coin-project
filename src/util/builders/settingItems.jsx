7338bf28b57e7bd6b188f83742a09a740d8df5d9
Joshua Permito <dwigoric@insanedevs.com>
2024-03-18 16:20:49 +0800

    refactor(SettingItem builder): support index- and name-based parameters

--- src/util/builders/settingItems.jsx
+++ src/util/builders/settingItems.jsx
@@ -1,12 +1,19 @@
 import SettingItem from '../../components/SettingItem/SettingItem.jsx'
 import settings from '../options.js'
 
-function settingItemsBuilder(tabIndex, submenuIndex, callback) {
-    const tab = settings[tabIndex]
-    return (tab.submenu[submenuIndex].items || []).map((setting, i) => {
+function settingItemsBuilder(firstLevel, secondLevel, callback) {
+    let tab
+    if (typeof firstLevel === 'string') tab = settings.find(setting => setting.name === firstLevel)
+    else tab = settings[firstLevel]
+
+    let submenu
+    if (typeof secondLevel === 'string') submenu = tab.submenu.find(setting => setting.name === secondLevel)
+    else submenu = tab.submenu[secondLevel]
+
+    return (submenu.items || []).map((setting, i) => {
         return (
             <SettingItem
-                key={`${tab.name}-${tab.submenu[submenuIndex].name}-${i}`}
+                key={`${tab.name}-${submenu.name}-${i}`}
                 text={setting.name}
                 value={setting.value}
                 type={setting.type}
